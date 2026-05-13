793360abbc686c1b9a481bcbc29e379bd6288995
diyk1111111 <dyiakdd@gmail.com>
2026-05-13 17:00:52 +0300

    fix router for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import GameView from '@/views/GameView.vue'
 import RulesView from '@/views/RulesView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
-    { path: '/',      name: 'home',  component: HomeView },
-    { path: '/game',  name: 'game',  component: GameView },
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/game', name: 'game', component: GameView },
     { path: '/rules', name: 'rules', component: RulesView },
   ]
 })
